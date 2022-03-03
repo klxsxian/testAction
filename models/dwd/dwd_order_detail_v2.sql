aabeebb7bdb3a5693d1bc0352eabecfdc36b9674
zhangjianwei <zhangjianwei@deepexi.com>
2022-03-03 18:25:19 +0800

    检查文件修改

--- models/dwd/dwd_order_detail_v2.sql
+++ models/dwd/dwd_order_detail_v2.sql
@@ -7,31 +7,36 @@
 }}
 
 with
-orders as  ( select * from {{ref('stg_order')}}   ),
-member as  ( select * from {{ref('stg_members')}} ),
-payment as ( select * from {{ref('stg_payment')}} )
+orders as ( select * from {{ref('stg_order')}}
+),
+
+member as ( select * from {{ref('stg_members')}}
+),
+
+payment as ( select * from {{ref('stg_payment')}}
+)
 
 select
     --
-    o.order_id,
-    o.member_id,
-    o.gmt_created as order_gmt_created,
-    o.gmt_updated as order_gmt_update,
-    o.real_amount,
+    orders.order_id,
+    orders.member_id,
+    orders.gmt_created as order_gmt_created,
+    orders.gmt_updated as order_gmt_update,
+    orders.real_amount,
     --
-    m.name,
-    m.address,
+    member.name,
+    member.address,
     --
     p.payment_id,
     p.amount as payment_amount,
     p.gmt_created as payment_gmt_created,
     p.gmt_updated as payment_gmt_updated
 
-from orders o
-         left join member m on o.member_id = m.member_id
-         left join payment p on o.order_id = p.order_id
+from orders
+left join member on orders.member_id = member.member_id
+left join payment as p on orders.order_id = p.order_id
 
-    {% if is_incremental() %}
-where 1=1
-  and p.gmt_updated >= (select max(payment_gmt_updated) from {{ this }})
+{% if is_incremental() %}
+where 1 = 1
+      and p.gmt_updated >= (select max(payment_gmt_updated) from {{ this }})
     {% endif %}
